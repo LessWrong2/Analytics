349b456f05766b590ff1a7da30f7c3d7e657a5c8
Ruben Bloom <rbloom@euclidanalytics.com>
2019-10-04 16:08:25 -0700

    Add function for parallelization!!

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
+import pandas as pd
+import numpy as np
 import datetime
 import pytz
 import logging
 from functools import wraps
 import configparser
+from multiprocessing import cpu_count, Pool
+
 
 def get_config_field(section, field):
     config = configparser.ConfigParser()
@@ -52,6 +56,16 @@ def mem_and_info(df):
           .merge(c, left_index=True, right_index=True)
           .sort_values('memory', ascending=False))
     print()  # creates space when running repeatedly
+
+
+def parallelize_dataframe(df, func, n_cores=None):
+
+    df_split = np.array_split(df, n_cores)
+    pool = Pool(n_cores)
+    df = pd.concat(pool.map(func, df_split))
+    pool.close()
+    pool.join()
+    return df
 
 
 def get_valid_users(dfu):
