29ef8d882d42700795b1b0de347b7c897ad64988
JP Addison <johnpaddison@gmail.com>
2021-08-25 12:03:16 +0100

    test get_post_id_from_path

--- db/migrations/20210824111234_author_metrics.sql
+++ db/migrations/20210824111234_author_metrics.sql
@@ -25,19 +25,19 @@ DECLARE
   post_id TEXT;
 BEGIN
   -- /posts/4fPxQjq6GFZgurSsf/room-for-other-things-how-to-adjust-if-ea-seems-overwhelming
-  -- /s/asdf/p/qwer/
+  -- /s/asdf/p/qwer
   post_id := (SELECT (regexp_matches(path, '^/posts/([a-zA-Z0-9]+)/'))[1]);
   if (post_id IS NOT NULL) THEN
     RETURN post_id;
   END IF;
-  post_id := (SELECT (regexp_matches(path, '^/s/[a-zA-Z0-9]+/p/([a-zA-Z0-9]+)/'))[1]);
+  post_id := (SELECT (regexp_matches(path, '^/s/[a-zA-Z0-9]+/p/([a-zA-Z0-9]+)'))[1]);
   RETURN post_id;
 END
 $$ LANGUAGE plpgsql IMMUTABLE STRICT;
 
 CREATE INDEX raw_event_type_post_id ON raw (event_type, get_post_id_from_path(event->>'path'));
 
-CREATE VIEW post_timer_events AS (
+CREATE VIEW post_timer_event AS (
   SELECT
     environment,
     -- event_type is always 'timerEvent'
@@ -58,11 +58,11 @@ CREATE VIEW post_timer_events AS (
 -- migrate:down
 -- #############################################################################
 
+DROP VIEW post_timer_event;
+
 DROP INDEX raw_event_type_post_id;
 
 DROP FUNCTION get_post_id_from_path(TEXT);
 
 DROP INDEX raw_event_type_timestamp;
 DROP INDEX raw_timestamp;
-
-DROP VIEW post_timer_events;
--- db/tests/author_metrics_tests.sql
+++ db/tests/author_metrics_tests.sql
@@ -1,6 +1,45 @@
 BEGIN;
 
-SELECT plan(1);
+TRUNCATE TABLE raw;
+
+CREATE TABLE get_post_id_from_path_test (
+  path text,
+  expected_post_id TEXT,
+  description TEXT
+);
+
+INSERT INTO get_post_id_from_path_test VALUES (
+  '/',
+  NULL,
+  'Nothing there'
+), (
+  '/posts/7irLXuSJGW7CjfunB/new-start-here',
+  '7irLXuSJGW7CjfunB',
+  'Simple post'
+), (
+  '/s/6gFGprxo27o7desCs/p/yBHqxKYS5rZot3gdq',
+  'yBHqxKYS5rZot3gdq',
+  'Post viewed in sequence view'
+);
+
+CREATE FUNCTION test_get_post_id_from_path ()
+RETURNS SETOF TEXT AS $$
+DECLARE
+  test get_post_id_from_path_test;
+BEGIN
+  FOR test IN (SELECT * FROM get_post_id_from_path_test) LOOP
+    RETURN NEXT is(
+      get_post_id_from_path(test.path),
+      test.expected_post_id,
+      test.description
+    );
+  END LOOP;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT plan(3);
+
+SELECT test_get_post_id_from_path();
 
 SELECT finish();
 
