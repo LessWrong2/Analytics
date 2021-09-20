ee4a54e0f38a23979dacd89c9824189b7576d0cd
JP Addison <johnpaddison@gmail.com>
2021-09-20 12:39:15 +0100

    add page_view view

--- db/migrations/20210917095409_post_metrics_2.sql
+++ db/migrations/20210917095409_post_metrics_2.sql
@@ -1,4 +1,6 @@
+-- #############################################################################
 -- migrate:up
+-- #############################################################################
 
 -- Before this we needed to run:
 -- forumanalytics=> update raw set event = (event#>>'{}')::jsonb;
@@ -51,15 +53,69 @@ CREATE VIEW event_timer_event AS (
 CREATE VIEW event_page_load_finished AS (
   SELECT
     environment,
-    -- event_type is always 'timerEvent'
+    -- event_type is always 'pageLoadFinished'
     timestamp,
-    event->>'path' AS path,
+    event->>'url' AS url,
     event->>'tabId' AS tab_id,
     event->>'userId' AS user_id,
-    (event->>'seconds')::INTEGER AS seconds,
     event->>'clientId' AS client_id,
-    (event->>'increment')::INTEGER AS increment,
-    get_post_id_from_path(event->>'url') AS post_id
+    event->>'referrer' AS referrer,
+    get_post_id_from_path(event->>'url') AS post_id,
+    (event->>'performance')::JSONB->>'memory' AS performance_memory,
+    -- https://stackoverflow.com/questions/16808486/explanation-of-window-performance-javascript
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'navigationStart'
+      AS performance_timing_navigation_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'redirectStart'
+      AS performance_timing_redirect_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'redirectEnd'
+      AS performance_timing_redirect_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'fetchStart'
+      AS performance_timing_fetch_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domainLookupStart'
+      AS performance_timing_domain_lookup_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domainLookupEnd'
+      AS performance_timing_domain_lookup_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'connectStart'
+      AS performance_timing_connect_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'secureConnectionStart'
+      AS performance_timing_secure_connection_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'connectEnd'
+      AS performance_timing_connect_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'requestStart'
+      AS performance_timing_request_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'responseStart'
+      AS performance_timing_response_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'unloadEventStart'
+      AS performance_timing_unload_event_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'unloadEventEnd'
+      AS performance_timing_unload_event_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'responseEnd'
+      AS performance_timing_response_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domLoading'
+      AS performance_timing_dom_loading,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domInteractive'
+      AS performance_timing_dom_interactive,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domContentLoadedEventStart'
+      AS performance_timing_dom_content_loaded_event_start,
+    ((event->>'perormance')::JSONB->>'timing')::JSONB->>'domContentLoadedEventEnd'
+      AS performance_timing_dom_content_loaded_event_end,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'domComplete'
+      AS performance_timing_dom_complete,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'loadEventStart'
+      AS performance_timing_load_event_start,
+    ((event->>'performance')::JSONB->>'timing')::JSONB->>'loadEventEnd'
+      AS performance_timing_load_event_end,
+    (event->>'performance')::JSONB->>'timeOrigin' AS performance_time_origin,
+    (event->>'browserProps')::JSONB->>'userAgent' AS browser_props_user_agent,
+    -- The following come from 'bowser'
+    ((event->>'browserProps')::JSONB->>'mobile')::BOOLEAN AS browser_props_mobile,
+    ((event->>'browserProps')::JSONB->>'tablet')::BOOLEAN AS browser_props_tablet,
+    ((event->>'browserProps')::JSONB->>'chrome')::BOOLEAN AS browser_props_chrome,
+    ((event->>'browserProps')::JSONB->>'firefox')::BOOLEAN AS browser_props_firefox,
+    ((event->>'browserProps')::JSONB->>'safari')::BOOLEAN AS browser_props_safari,
+    (event->>'browserProps')::JSONB->>'osname' AS browser_props_osname,
+    ((event->>'browserProps')::JSONB->>'blocks')::JSONB->>'blocksGA' AS browser_props_blocks_ga,
+    ((event->>'browserProps')::JSONB->>'blocks')::JSONB->>'blocksGTM' AS browser_props_blocks_gtm
   FROM raw
   WHERE event_type = 'pageLoadFinished'
 );
@@ -67,7 +123,7 @@ CREATE VIEW event_page_load_finished AS (
 CREATE VIEW event_navigate AS (
   SELECT
     environment,
-    -- event_type is always 'timerEvent'
+    -- event_type is always 'navigate'
     timestamp,
     event->>'tabId' AS tab_id,
     event->>'userId' AS user_id,
@@ -80,8 +136,41 @@ CREATE VIEW event_navigate AS (
   WHERE event_type = 'navigate'
 );
 
---------------------------------------------------------------------------------
+CREATE VIEW page_view AS (
+  SELECT
+    environment,
+    event_type,
+    timestamp,
+    tab_id,
+    user_id,
+    client_id,
+    post_id
+  FROM (
+    SELECT
+      environment,
+      'page_load_finished' AS event_type,
+      timestamp,
+      tab_id,
+      user_id,
+      client_id,
+      post_id
+    FROM event_page_load_finished
+    UNION ALL
+    SELECT
+      environment,
+      'navigate' AS event_type,
+      timestamp,
+      tab_id,
+      user_id,
+      client_id,
+      to_post_id AS post_id
+    FROM event_navigate
+  ) a
+);
+
+-- #############################################################################
 -- migrate:down
+-- #############################################################################
 
 -- DROP INDEX raw_event_type_post_id_timer_event;
 -- DROP INDEX raw_event_type_post_id_page_load_finished;
@@ -105,6 +194,8 @@ END
 $$ LANGUAGE plpgsql IMMUTABLE STRICT;
 
 -- CREATE INDEX raw_event_type_post_id ON raw (event_type, get_post_id_from_path(event->>'path'));
+
+DROP VIEW page_view;
 
 DROP VIEW event_timer_event;
 DROP VIEW event_page_load_finished;
